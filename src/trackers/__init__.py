3686fca1a570c527990a854be3a7092d7bce8873
jxk6575 <153412013+jxk6575@users.noreply.github.com>
2024-12-18 23:02:58 +0800

    Add files via upload

--- src/trackers/__init__.py
+++ src/trackers/__init__.py
@@ -3,5 +3,6 @@
 from .sort.sort_tracker import SORTTracker
 from .deep_sort.deep_sort_tracker import DeepSORTTracker
 from .byte_track.byte_tracker import ByteTracker
+from .hybrid_track.hybrid_tracker import HybridTracker
 
-__all__ = ['SORTTracker', 'DeepSORTTracker', 'ByteTracker']
+__all__ = ['SORTTracker', 'DeepSORTTracker', 'ByteTracker', 'HybridTracker']
